bf9a991f0a0ff51dd0acfe125d810ea813ccf38c
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-14 09:20:48 -0800

    Add house search to home page

--- real-estate-client/src/routes/Home.jsx
+++ real-estate-client/src/routes/Home.jsx
@@ -1,9 +1,13 @@
 import { HouseListing } from "../components/HouseListing"
+import { HomeSearch } from "../components/HomeSearch"
 
 export function Home(props) {
     const { homesData } = props;
     console.log(homesData)
     return (
-        <HouseListing houseData = {homesData[0]} />
+        <>
+            <HomeSearch />
+            <HouseListing houseData = {homesData[0]} />
+        </>
     )
 }
