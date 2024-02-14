f06eec8c233e69d3a5e61868bdc16205d52de593
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-14 12:20:01 -0800

    Set up the database

---
+++ real-estate-server/db.js
@@ -0,0 +1,105 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/realestate.db');
+var crypto = require('crypto');
+
+// based on the example found here: https://github.com/passport/todos-express-password/blob/master/db.js
+
+function createUser(username, password) {
+    let salt = crypto.randomBytes(16);
+    db.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+        username,
+        crypto.pbkdf2Sync(password, salt, 310000, 32, 'sha256'),
+        salt
+      ]);
+}
+
+function createSampleHouse() {
+    db.run("INSERT OR IGNORE INTO houses ( \
+        mls_num, \
+        street1, \
+        street2, \
+        city, \
+        state, \
+        zip_code, \
+        neighborhood, \
+        sales_price, \
+        date_listed, \
+        bedrooms, \
+        photos, \
+        bathrooms, \
+        garage_size, \
+        square_feet, \
+        lot_size, \
+        description) \
+            VALUES ( \
+                ?,  \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ?, \
+                ? \
+            );", [
+                2,
+                "2123 Main St",
+                "",
+                "Anytown",
+                "TX",
+                12345,
+                "Anytown Neighborhood",
+                100000,
+                "2021-01-01",
+                3,
+                "photo1.jpg,photo2.jpg",
+                2,
+                2,
+                1500,
+                0.25,
+                "A great house in a great neighborhood"
+            ]
+    )
+}
+
+db.serialize(() => {
+    // Create the database schema for the real estate app
+    db.run("CREATE TABLE IF NOT EXISTS users ( \
+        id INTEGER PRIMARY KEY, \
+        username TEXT UNIQUE, \
+        hashed_password BLOB, \
+        salt BLOB \
+    )");
+
+    db.run("CREATE TABLE IF NOT EXISTS houses ( \
+        mls_num INTEGER PRIMARY KEY, \
+        street1 TEXT, \
+        street2 TEXT, \
+        city TEXT, \
+        state TEXT, \
+        zip_code INTEGER, \
+        neighborhood TEXT, \
+        sales_price REAL, \
+        date_listed TEXT, \
+        bedrooms INTEGER, \
+        photos TEXT, \
+        bathrooms REAL, \
+        garage_size REAL, \
+        square_feet REAL, \
+        lot_size REAL, \
+        description TEXT \
+    );")
+
+    createUser("admin", "password")
+    createSampleHouse()
+
+})
+
+module.exports = db;
