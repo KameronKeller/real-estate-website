a213505d2621a91442bbde2568dc15275781a075
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-14 09:03:27 -0800

    Create root html structure

--- real-estate-client/src/routes/Root.jsx
+++ real-estate-client/src/routes/Root.jsx
@@ -1,5 +1,21 @@
-export function Root() {
+import { Outlet, } from "react-router-dom";
+
+export function Root(props) {
+    const { children } = props;
     return(
-        <p>hello</p>
+        <>
+            <nav>
+                <p>website name</p>
+                <button>login</button>
+            </nav>
+            <main>
+                { children || <Outlet /> }
+            </main>
+            <footer>
+                <p>footer</p>
+            </footer>
+            {/* children will display the error page if an invalid route is requested */}
+            
+        </>
     )
 }
