90a4d9a185b418118b6ea56e7d57fbc4de7996df
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-15 07:03:41 -0800

    Remove unneeded assets

--- real-estate-client/src/main.jsx
+++ real-estate-client/src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import App from './App.jsx'
 import './index.css'
-
-import sample_home from './assets/home_sample.json';
 
 import {
   Root,
