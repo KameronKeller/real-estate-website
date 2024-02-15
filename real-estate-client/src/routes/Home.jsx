9b41e73a7e51044dd1ffb3298b581dbf685b6613
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-15 07:04:34 -0800

    Pass homes data to house listing for delete function

--- real-estate-client/src/routes/Home.jsx
+++ real-estate-client/src/routes/Home.jsx
@@ -1,24 +1,25 @@
 import { HouseListing } from "../components/HouseListing"
 import { HomeSearch } from "../components/HomeSearch"
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
 
 export function Home() {
-    const [homesData, setHomesData] = useState([]);
+    const [homesData, setHomesData] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:3000/homes')
           .then(response => response.json())
           .then(data => setHomesData(data))
-      }, []);
+      }, [])
 
 
     return (
         <>
+            <h3>Search for Homes:</h3>
             <HomeSearch />
 
             {homesData.map((house) => {
-                return <HouseListing key={house.mls_num} houseData={house} />
+                return <HouseListing key={house.mls_num} houseData={house} homesData={homesData} setHomesData={setHomesData} />
             })}
         </>
     )
