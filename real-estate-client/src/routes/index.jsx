15742ab46c349adc17131f27511fe3d5e82552b8
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-15 07:05:03 -0800

    Remove semicolons

--- real-estate-client/src/routes/index.jsx
+++ real-estate-client/src/routes/index.jsx
@@ -1,4 +1,4 @@
-export { Root } from './Root';
-export { Home } from './Home';
-export { Login } from './Login';
-export { AddHome } from './AddHome';
+export { Root } from './Root'
+export { Home } from './Home'
+export { Login } from './Login'
+export { AddHome } from './AddHome'
