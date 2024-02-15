4e01b2b10748d2d5e354d7782648c7249dc259b5
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-15 07:04:42 -0800

    Add login functionality

--- real-estate-client/src/routes/Login.jsx
+++ real-estate-client/src/routes/Login.jsx
@@ -1,11 +1,53 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export function Login() {
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        const response = await fetch('http://localhost:3000/login/password', {
+            mode: 'cors',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+            })
+
+            if (response.ok) {
+                console.log('Login successful')
+                // save session id
+                // redirect to home page
+                navigate("/")
+            } else {
+                console.log('Login failed')
+            }
+
+    }
+
     return (
-        <form>
-            <label htmlFor="username">Username:</label><br />
-            <input type="text" id="username" name="username" /><br />
-            <label htmlFor="password">Password:</label><br />
-            <input type="password" id="password" name="password" />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
             <input type="submit" value="Submit" />
         </form>
     )
